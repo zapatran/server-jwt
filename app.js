a0ef8f46b698c262887a6702b578d7a23c649277
zapatran <zapatran@users.noreply.github.com>
2022-02-26 12:58:18 +0100

    add cors

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-require("dotenv/config");
-require("./db");
-const express = require("express");
+require('dotenv/config');
+require('./db');
+const express = require('express');
+const cors = require('cors');
 
-const { isAuthenticated } = require("./middleware/jwt.middleware");
-const allRoutes = require("./routes");
-const authRouter = require("./routes/auth.routes");
-const protectedRoute = require("./routes/protected.routes");
+const { isAuthenticated } = require('./middleware/jwt.middleware');
+const allRoutes = require('./routes');
+const authRouter = require('./routes/auth.routes');
+const protectedRoute = require('./routes/protected.routes');
 
 const app = express();
 
-require("./config")(app);
+require('./config')(app);
 
-app.use("/api", allRoutes);
-app.use("/api/protected", isAuthenticated, protectedRoute);
-app.use("/auth", authRouter);
+app.use(cors({ origin: process.env.ORIGIN || 'http://localhost:3000' }));
 
-require("./error-handling")(app);
+app.use('/api', allRoutes);
+app.use('/api/protected', isAuthenticated, protectedRoute);
+app.use('/auth', authRouter);
+
+require('./error-handling')(app);
 
 module.exports = app;
